dc93e3215dad309ffec5ea4c7ecbaa6412cded7c
benjamin.wang <benjamin.wang@nl.abnamro.com>
2020-05-29 16:02:36 +0200

    support mlflow

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,15 +3,24 @@ Borrowed from cookiecutter-django: https://github.com/pydanny/cookiecutter-djang
 """
 
 import os
+import shutil
 
 
 def remove_azure_pipeline_yaml_file():
     os.remove("azure-pipelines.yml")
 
 
+def remove_mlflow_dir():
+    shutil.rmtree("mlflow")
+
+
 def main():
     if "{{ cookiecutter.use_azure_pipeline }}".lower() == "n":
         remove_azure_pipeline_yaml_file()
+
+    if "{{ cookiecutter.use_mlflow }}".lower() == "n":
+        remove_mlflow_dir()
+
     print("Your project {{ cookiecutter.project_name }} is initiated successfully, enjoy the journey!")
 
 
